00329aef7c726cf58c3d8f4f9c543d764e768bf1
Marijn Haverbeke <marijnh@gmail.com>
2013-03-20 17:41:40 +0100

    Fix obvious infinite loop bug

--- tern.js
+++ tern.js
@@ -210,10 +210,10 @@
   }
 
   function findMatchingPosition(line, file, near) {
-    var pos = 0, closest = null;
+    var pos = Math.max(0, near - 500), closest = null;
     if (!/^\s*$/.test(line)) for (;;) {
-      var found = file.indexOf(line, pos - 500);
-      if (found < 0 || found > pos + 500) break;
+      var found = file.indexOf(line, pos);
+      if (found < 0 || found > near + 500) break;
       if (closest == null || Math.abs(closest - near) > Math.abs(found - near))
         closest = found;
       pos = found + line.length;
