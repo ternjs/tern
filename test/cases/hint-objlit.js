382fc633403da97fbeb5f41ca4e4591a21d8fd60
Marijn Haverbeke <marijnh@gmail.com>
2014-09-22 15:16:10 +0200

    Add support for object literal name completion
    Depends on some yet-to-be-released Tern patches

---
+++ test/cases/hint-objlit.js
@@ -0,0 +1,11 @@
+var person = {name: "harry", born: 1980};
+
+function age(person) { return 2014 - person.age; }
+function ages(persons) return persons.map(age); }
+
+age(person);
+ages([person, person]);
+
+age({n //+ name
+
+ages([{b //+ born
