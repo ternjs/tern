a369a21b6c5eafc51fdd156080d3e9e2891ba554
Marijn Haverbeke <marijnh@gmail.com>
2014-12-15 17:33:33 +0100

    Add more tests for object properties, fix small issue

--- test/cases/hint-objlit.js
+++ test/cases/hint-objlit.js
@@ -1,3 +1,5 @@
+var poison = {focus: 1, blah: 2, nothing: 3};
+
 var person = {name: "harry", born: 1980};
 
 function age(person) { return 2014 - person.age; }
@@ -9,3 +11,37 @@ ages([person, person]);
 age({n //+ name
 
 ages([{b //+ born
+
+var example = {
+  // The foo
+  foo: 10,
+  // The food
+  food: 20,
+  // The bar
+  bar: true
+};
+
+function takes(example) { return example; }
+takes(example);
+
+takes({
+  fo //+ foo, food
+});
+
+takes({
+  _1: "long long long long long long long long long long long long long long long long long long string",
+  noise: 10,
+  food //<doc: The food
+  bar: false,
+  _2: "long long long long long long long long long long long long long long long long long long string"
+});
+
+takes({
+  _1: "long long long long long long long long long long long long long long long long long long string",
+  noise: 10,
+  b //+ bar
+  _2: "long long long long long long long long long long long long long long long long long long string"
+});
+
+takes({
+  bar //<: bool
