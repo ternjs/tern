62485fd9d70cdf06471d6513dc9a092cccf9f49f
Marijn Haverbeke <marijnh@gmail.com>
2015-04-29 14:16:53 +0200

    Add support for defineProperties
    Issue #377

--- test/cases/defineProperty.js
+++ test/cases/defineProperty.js
@@ -13,3 +13,13 @@ o.prop1; //: string
 o.prop2; //: number
 
 o.prop1; //doc: Docstring for prop1
+
+var o2 = {};
+
+Object.defineProperties(o2, {
+  prop1: {get: function() { return 2; }},
+  prop2: {value: true}
+});
+
+o2.prop1; //: number
+o2.prop2; //: bool
