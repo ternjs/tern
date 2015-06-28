21c5ce41c85f18d65a5fa288c9eb69f4bf862deb
Angelo <angelo.zerr@gmail.com>
2015-06-28 16:22:45 +0200

    [requirejs plugin] Completion for required/defined modules
    This PR provides completion for RequireJS modules inside string array
    dependencies (which looks like to node tern plugin).

--- plugin/requirejs.js
+++ plugin/requirejs.js
@@ -248,7 +248,8 @@
       passes: {
         preCondenseReach: preCondenseReach,
         postLoadDef: postLoadDef,
-        typeAt: findTypeAt
+        typeAt: findTypeAt,
+        completion: findCompletions
       }
     };
   });
@@ -270,6 +271,93 @@
     return type;
   }
 
+  function findCompletions(file, query) {
+    var wordEnd = tern.resolvePos(file, query.end);
+    var callExpr = infer.findExpressionAround(file.ast, null, wordEnd, file.scope, "CallExpression");
+    if (!callExpr) return;
+    var callNode = callExpr.node;
+    if (callNode.callee.type != "Identifier" || 
+        !(callNode.callee.name == "define" || callNode.callee.name == "require" || callNode.callee.name == "requirejs")||
+        callNode.arguments.length < 1 || callNode.arguments[0].type != "ArrayExpression") return;
+    var argNode = findRequireModule(callNode.arguments[0].elements, wordEnd);
+    if (!argNode) return;
+    var word = argNode.raw.slice(1, wordEnd - argNode.start), quote = argNode.raw.charAt(0);
+    if (word && word.charAt(word.length - 1) == quote)
+      word = word.slice(0, word.length - 1);
+    var completions = completeModuleName(query, file, word);
+    if (argNode.end == wordEnd + 1 && file.text.charAt(wordEnd) == quote)
+      ++wordEnd;
+    return {
+      start: tern.outputPos(query, file, argNode.start),
+      end: tern.outputPos(query, file, wordEnd),
+      isProperty: false,
+      isObjectKey: false,
+      completions: completions.map(function(rec) {
+        var name = typeof rec == "string" ? rec : rec.name;
+        var string = JSON.stringify(name);
+        if (quote == "'") string = quote + string.slice(1, string.length -1).replace(/'/g, "\\'") + quote;
+        if (typeof rec == "string") return string;
+        rec.displayName = name;
+        rec.name = string;
+        return rec;
+      })
+    };
+  }
+  
+  function findRequireModule(argsNode, wordEnd) {
+    for (var i = 0; i < argsNode.length; i++) {
+      var argNode = argsNode[i];
+      if (argNode.type == "Literal" && typeof argNode.value == "string" &&
+          argNode.start < wordEnd && argNode.end > wordEnd) return argNode;
+    }
+  }
+  
+  function completeModuleName(query, file, word) {
+    var completions = [];
+    var cx = infer.cx(), server = cx.parent, data = server._requireJS;
+    var currentFile = data.currentFile;
+    var wrapAsObjs = query.types || query.depths || query.docs || query.urls || query.origins;
+    var base = data.options.baseURL || "";
+    if (base && base.charAt(base.length - 1) != "/") base += "/";
+    
+    function maybeSet(obj, prop, val) {
+      if (val != null) obj[prop] = val;
+    }
+    
+    function gather(modules) {
+      for (var name in modules) {
+        if (name == currentFile || !modules[name].getType()) continue;
+
+        var moduleName = name.substring(base.length, name.length);
+        if (moduleName &&
+            !(query.filter !== false && word &&
+              (query.caseInsensitive ? moduleName.toLowerCase() : moduleName).indexOf(word) !== 0)) {
+          var rec = wrapAsObjs ? {name: moduleName} : moduleName;
+          completions.push(rec);
+
+          if (query.types || query.docs || query.urls || query.origins) {
+            var val = modules[name];
+            infer.resetGuessing();
+            var type = val.getType();
+            rec.guess = infer.didGuess();
+            if (query.types)
+              rec.type = infer.toString(val);
+            if (query.docs)
+              maybeSet(rec, "doc", val.doc || type && type.doc);
+            if (query.urls)
+              maybeSet(rec, "url", val.url || type && type.url);
+            if (query.origins)
+              maybeSet(rec, "origin", val.origin || type && type.origin);
+          }
+        }
+      }
+    }
+
+    if (query.caseInsensitive) word = word.toLowerCase();
+    gather(data.interfaces);
+    return completions;
+  }
+  
   var defs = {
     "!name": "requirejs",
     "!define": {
