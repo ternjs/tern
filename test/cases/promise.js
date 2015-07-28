c5df4871f10905ac704793d5a454b2462f5e486d
Marijn Haverbeke <marijnh@gmail.com>
2015-07-28 16:48:57 +0200

    Update Promise.prototype.then implementation
    Issue #591

--- test/cases/promise.js
+++ test/cases/promise.js
@@ -7,12 +7,17 @@ var p = new Promise(function(accept, reject) {
 
 p.then(function(value) {
   value; //:: {x: number}
+}).then(function(value) {
+  value; //:: {x: number}
 });
 
 var p2 = new Promise(function(acc) { acc("hi"); });
 
 Promise.all([p2]).then(function(value) {
   value; //: string
+  return Promise.resolve(33);
+}).then(function(value) {
+  value; //: number
 });
 
 var p3 = Promise.resolve(10);
@@ -20,4 +25,7 @@ p3.value; //: number
 
 p3.then(function(value) {
   value; //: number
+  return true;
+}).then(function(value) {
+  value; //: bool
 });
