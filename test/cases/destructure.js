030aaae02008596463e3db6f697596126cbe34a4
Marijn Haverbeke <marijnh@gmail.com>
2015-08-07 11:14:52 +0200

    Implement destructuring

---
+++ test/cases/destructure.js
@@ -0,0 +1,26 @@
+let [a] = [1, 2]
+a //: number
+
+let {c, d} = {c: "hello", d: true}
+c //: string
+d //: bool
+
+function foo([e], {f=1}, {g}, ...h) {
+  e //: bool
+  f //: number
+  g //: string
+  h //: [number]
+}
+// Check for argument leakage
+e //: ?
+g //: ?
+
+foo([false], blah(), {g: "hello"}, 20)
+
+let i, j
+({i, j, k: [l, ...m]} = {i: 1, j: false, k: ["a", "b"]})
+
+i //: number
+j //: bool
+l //: string
+m //: [string]
