3af85d8ce014c4048930d02400bb63156f35c1b1
Marijn Haverbeke <marijnh@gmail.com>
2015-08-07 15:31:57 +0200

    Basic class support

---
+++ test/cases/class.js
@@ -0,0 +1,30 @@
+class Point2 {
+  constructor(x, y) { this.x = x; this.y = y }
+  plus(pt) {
+    this //: Point2
+    pt //: Point2
+    return new Point2(this.x + pt.x, this.y + pt.y)
+  }
+  get xx() { return this.x }
+  quux() { return 1 }
+  static origin() { return new Point2(0, 0) }
+}
+
+var Point3 = class extends Point2 {
+  constructor(x, y, z) { super(x, y); this.z = z }
+  foobar() { return true }
+}
+
+var p1 = new Point2(1, 2)
+p1.x //: number
+p1 //: Point2
+var p2 = Point2.origin()
+p2 //: Point2
+p1.plus(p2) //: Point2
+p1.foobar() //: ?
+p1.xx //: number
+
+var p3 = new Point3(0, 0, 5)
+p3 //: Point3
+p3.quux() //: number
+p3.foobar() //: bool
