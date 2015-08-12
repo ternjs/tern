f40ee51e32d8508120a7d88656fcfe8b5675c43c
Marijn Haverbeke <marijnh@gmail.com>
2015-08-12 17:04:13 +0200

    Add support for for/of

---
+++ test/cases/for_of.js
@@ -0,0 +1,18 @@
+var myIter = {
+  [Symbol.iterator]() {
+    return {
+      next() {
+        return {value: {a: 1, b: true}, done: false}
+      }
+    }
+  }
+}
+
+for (var hello of myIter) {
+  hello //:: {a: number, b: bool}
+}
+
+for (var {a, b} of myIter) {
+  a //: number
+  b //: bool
+}
