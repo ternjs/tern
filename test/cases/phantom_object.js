eeb2d0c7dbcdd394a8eb87ade70f78be0b30cc22
Marijn Haverbeke <marijnh@gmail.com>
2015-08-31 13:45:30 +0200

    Add phantom object test case

---
+++ test/cases/phantom_object.js
@@ -0,0 +1,11 @@
+var mod = window.mymodule
+
+mod.func = function() { return 10 }
+
+mod.func //: fn() -> number
+
+var otherMod = window.yourmodule.theirmodule
+
+otherMod.c = 10
+
+otherMod.c //: number
