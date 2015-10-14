27f91d6d22f7b006a0a4147e169d94a48242a43f
Marijn Haverbeke <marijnh@gmail.com>
2015-10-14 11:36:50 +0200

    Fix capitalized constructor heuristic regexp
    Issue #658

--- plugin/doc_comment.js
+++ plugin/doc_comment.js
@@ -348,7 +348,7 @@
   }
 
   function maybeInstance(type, path) {
-    if (type instanceof infer.Fn && /^[A-Z]/.test(path)) {
+    if (type instanceof infer.Fn && /(?:^|\.)[A-Z][^\.]*$/.test(path)) {
       var proto = type.getProp("prototype").getObjType();
       if (proto instanceof infer.Obj) return infer.getInstance(proto);
     }
