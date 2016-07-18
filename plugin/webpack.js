bc255057b0f993449ab3a6b7ee30fb9aed13bfbd
chemzqm <chemzqm@gmail.com>
2016-07-18 15:28:56 +0800

    support webpack.config.js for webpack plugin

--- plugin/webpack.js
+++ plugin/webpack.js
@@ -4,21 +4,39 @@ if (typeof exports != "object" || typeof module != "object")
 var infer = require("../lib/infer");
 var tern = require("../lib/tern");
 require("./commonjs");
+require("./es_modules");
 
+var fs = require('fs');
 var path = require("path");
-var req = require("enhanced-resolve");
-var DirectoryDescriptionFileFieldAliasPlugin = require("enhanced-resolve/lib/DirectoryDescriptionFileFieldAliasPlugin");
+var ResolverFactory = require("enhanced-resolve").ResolverFactory;
+var SyncNodeJsInputFileSystem = require("enhanced-resolve/lib/SyncNodeJsInputFileSystem");
 
-var resolver = new req.Resolver(new req.SyncNodeJsInputFileSystem());
-resolver.apply(
-  new DirectoryDescriptionFileFieldAliasPlugin("package.json", "browser"),
-  new req.ModulesInDirectoriesPlugin("node", ["node_modules"]),
-  new req.ModuleAsFilePlugin("node"),
-  new req.ModuleAsDirectoryPlugin("node"),
-  new req.DirectoryDescriptionFilePlugin("package.json", ["main", "browser"]),
-  new req.DirectoryDefaultFilePlugin(["index"]),
-  new req.FileAppendPlugin(["", ".js"])
-);
+var file = process.cwd() + '/webpack.config.js'
+var resolveConfig = fs.existsSync(file) ? require(file).resolve : null
+
+var config = {
+  unsafeCache: true,
+  modules: ["node_modules"],
+  extensions: [".js", ".jsx", ".json"],
+  aliasFields: ["browser"],
+  mainFields: ["browser", "web", "browserify", "main"],
+  fileSystem: new SyncNodeJsInputFileSystem()
+}
+
+if (resolveConfig) {
+  Object.keys(resolveConfig).forEach(function (key) {
+    if (key === 'packageMains') {
+      config.mainFields = resolveConfig[key]
+    } else if (key === 'root') {
+      config.modules.unshift(resolveConfig[key])
+    } else {
+      config[key] = resolveConfig[key]
+    }
+  })
+}
+
+var resolver = ResolverFactory.createResolver(config);
+
 
 function resolve(name, parentFile) {
   var resolved = resolveToFile(name, parentFile)
@@ -26,16 +44,18 @@ function resolve(name, parentFile) {
 }
 
 function resolveToFile(name, parentFile) {
-  try {
     var projectDir = infer.cx().parent.projectDir;
     var fullParent = path.resolve(projectDir, parentFile);
-    return resolver.resolveSync(fullParent, name);
-  } catch(e) {
-    return '';
-  }
+    try {
+      return resolver.resolveSync({}, path.dirname(fullParent), name);
+    } catch(e) {
+      console.log(e.stack)
+      return ''
+    }
 }
 
 tern.registerPlugin("webpack", function(server) {
   server.loadPlugin("commonjs")
+  server.loadPlugin("es_modules")
   server.mod.modules.resolvers.push(resolve)
 })
