1cba2cd68004905a8b68f4c061a43261e11b75f2
Ralf Kistner <ralf@journeyapps.com>
2016-08-12 17:03:07 +0200

    Some fixes for Promise.resolve behavior.

--- test/cases/promise.js
+++ test/cases/promise.js
@@ -26,3 +26,29 @@ p3.then(function(value) {
 }).then(function(value) {
   value; //: bool
 });
+
+var p4 = Promise.resolve(Promise.resolve(10));
+p4.then(function(value) {
+  value; //: number
+});
+
+var arg5 = 1 < 2 ? Promise.resolve(10) : 20;
+var p5 = Promise.resolve(arg5);
+p5.then(function(value) {
+  value; //: number
+});
+
+var p6 = Promise.resolve('t').then(function() {
+  return 1 < 2 ? Promise.resolve(10) : 20;
+}).then(function(value) {
+  value; //: number
+});
+
+var p7 = Promise.resolve().then(function() {
+  return 20;
+}).then(function(value) {
+  value; //: number
+});
+
+var p8 = Promise.resolve();
+//: Promise
