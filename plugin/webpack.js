b26e513f7bb8c7bb3509b7ce7066212673cef285
Eloy Toro <eloytoro@gmail.com>
2016-12-24 01:52:35 -0600

    allow for `modules` options in the webpack plugin

--- plugin/webpack.js
+++ plugin/webpack.js
@@ -11,16 +11,16 @@ var path = require("path");
 var ResolverFactory = require("enhanced-resolve").ResolverFactory;
 var SyncNodeJsInputFileSystem = require("enhanced-resolve/lib/SyncNodeJsInputFileSystem");
 
-function getResolver(configPath) {
+function getResolver(modules, configPath) {
   var config = {
     unsafeCache: true,
-    modules: ["node_modules"],
+    modules: modules || ["node_modules"],
     extensions: [".js", ".jsx", ".json"],
     aliasFields: ["browser"],
     mainFields: ["browser", "web", "browserify", "main"],
     fileSystem: new SyncNodeJsInputFileSystem()
   }
-  var webpackConfig = fs.existsSync(configPath) ? require(configPath) : null
+  var webpackConfig = (configPath && fs.existsSync(configPath)) ? require(configPath) : null
   if (typeof webpackConfig === 'function') {
     webpackConfig = webpackConfig()
   }
@@ -55,8 +55,9 @@ function resolveToFile(resolver, name, parentFile) {
 
 tern.registerPlugin("webpack", function(server, options) {
   var configPath = options.configPath || './webpack.config.js'
+  var modules = options.modules || ['node_modules']
   configPath = path.resolve(server.options.projectDir, configPath)
-  var resolver = getResolver(configPath)
+  var resolver = getResolver(modules, configPath)
   server.loadPlugin("commonjs")
   server.loadPlugin("es_modules")
   server.mod.modules.resolvers.push(function (name, parentFile) {
