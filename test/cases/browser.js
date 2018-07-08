2ba222667bf31f265bf57ce9c7a5bcfe3fa1607d
othree <othree@gmail.com>
2018-07-08 16:08:03 +0800

    Update browser.console defs

--- test/cases/browser.js
+++ test/cases/browser.js
@@ -9,3 +9,5 @@ newElt.style.border; //: string
 var e_which;
 window.addEventListener("mousemove", function(e) { e_which = e.which; });
 e_which; //: number
+
+console. //+ assert, clear, count, debug, dir, error, group, groupCollapsed, groupEnd, info, log, table, time, timeEnd, trace, warn
