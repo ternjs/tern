ab7f0be74001037f6a9310098e6dada981b0c756
othree <othree@gmail.com>
2018-07-13 17:12:47 +0800

    Add arithmetic operation test

---
+++ test/cases/arithmetic_ops.js
@@ -0,0 +1,18 @@
+
+var a = '1' + 1;
+a; //: string
+
+var b = '1' - 1;
+b; //: number
+
+var c = '1' * 1;
+c; //: number
+
+var d = '1' / 1;
+d; //: number
+
+var e = '1' % 1;
+e; //: number
+
+var f = '1' ** 1;
+f; //: number
