ternjs/tern

commit 3f4794d39aa87163cb96a43fa48635a0a6f16b95

Clear scopes on AST before analysis
Issue #819

lib/infer.js
@@ -1160,6 +1160,13 @@
   });
   scopeGatherer.ForOfStatement = scopeGatherer.ForInStatement
 
+  function rmScope(node) { if (node.scope) node.scope = null }
+  var scopeClearer = {BlockStatement: rmScope, Function: rmScope, CatchClause: rmScope,
+                      ForInStateMent: rmScope, ForStatement: rmScope}
+  exports.clearScopes = function(ast) {
+    walk.simple(ast, scopeClearer)
+  };
+
   // CONSTRAINT GATHERING PASS
 
   var propName = exports.propName = function(node, inferInScope) {

lib/tern.js
@@ -152,7 +152,10 @@
       this.budgets = Object.create(null);
       for (var i = 0; i < this.files.length; ++i) {
         var file = this.files[i];
-        file.scope = null;
+        if (file.scope) {
+          infer.clearScopes(file.ast);
+          file.scope = null;
+        }
       }
       this.signal("postReset");
     },
@@ -294,6 +297,7 @@
       if (text != null) {
         if (known.scope) {
           srv.needsPurge.push(name);
+          infer.clearScopes(known.ast);
           known.scope = null;
         }
         updateText(known, text, srv);

commit 1b158eb39c3f313725a594ded46f7d5b46aa61cc

Fix typo in property name
Fix casing of the ForInStatement property. Bug appears to have been
introduced in commit 3f4794d ("Clear scopes on AST before analysis")
from December 2016.

lib/infer.js
@@ -1178,7 +1178,7 @@
 
   function rmScope(node) { if (node.scope) node.scope = null }
   var scopeClearer = {BlockStatement: rmScope, Function: rmScope, CatchClause: rmScope,
-                      ForInStateMent: rmScope, ForStatement: rmScope};
+                      ForInStatement: rmScope, ForStatement: rmScope};
   exports.clearScopes = function(ast) {
     walk.simple(ast, scopeClearer);
   };